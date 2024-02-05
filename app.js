cdf866df0bfe869d1ecf5721e5e05edce85ad734
raulmaia25 <raul_maia25@yahoo.com.br>
2024-02-05 08:11:25 -0300

    att

---
+++ app.js
@@ -0,0 +1,24 @@
+const texto = document.getElementById("recebido");
+
+function cripto() {
+  let cripto = texto.value
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("a", "ai")
+    .replaceAll("o", "ober")
+    .replaceAll("u", "ufat");
+
+  document.getElementById("resultado").innerHTML = cripto;
+}
+
+function descripto() {
+  let desCripto = texto.value
+    .replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ai", "a")
+    .replaceAll("ober", "o")
+    .replaceAll("ufat", "u");
+
+  document.getElementById("resultado").innerHTML = desCripto;
+  desCripto = desCripto.style;
+}
